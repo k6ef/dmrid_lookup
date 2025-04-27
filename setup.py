943a181fd77eb0862517ca2376938e014cbbda35
Mark Cohen <mark@petrichor.local>
2025-04-27 15:27:39 -0700

    fix: force new release 1.0.8

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
 setup(
-    name="dmrid_lookup",
-    version="1.0.8",
+    name="dmrid-lookup",
+    version="1.0.8",  # semantic-release will bump this automatically
     py_modules=["dmrid_lookup"],
     install_requires=[
         "requests",
@@ -13,4 +13,20 @@ setup(
             "dmrid_lookup=dmrid_lookup:main",
         ],
     },
+    author="Your Name",
+    author_email="k6ef@k6ef.net",
+    description="A CLI tool to lookup DMR ID from callsign or vice-versa using radioid.net",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/k6ef/dmrid-lookup",  # Update to your actual repo
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Environment :: Console",
+        "Intended Audience :: End Users/Desktop",
+        "Topic :: Communications :: Ham Radio",
+    ],
+    python_requires=">=3.7",
 )
+
