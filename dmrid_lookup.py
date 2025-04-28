d67cdca9ee7ad851baaf2754306ca0fe8f8e57da
Mark Cohen <k6ef@k6ef.net>
2025-04-27 21:12:15 -0700

    fix: updating automation

--- dmrid_lookup.py
+++ dmrid_lookup.py
@@ -144,7 +144,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-# fixing for semantic-release trigger
-# fixing for semantic-release trigger
-# fixing for semantic-release trigger
-# fixing for semantic-release trigger
