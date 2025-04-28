7a42dc837fb9a1eb2724ae2f22aefb11c0b817e0
Mark Cohen <k6ef@k6ef.net>
2025-04-28 15:10:08 -0700

    chore: bump version to 1.0.14

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="dmrid_lookup",
-    version="1.0.13",
+    version="1.0.14",
     author="Mark Cohen",
     author_email="k6ef@k6ef.net",
-    description="A tool to lookup DMR IDs and callsigns",
+    description="A Python package for looking up DMR IDs from radioid.net",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/k6ef/dmrid_lookup",
@@ -18,7 +18,11 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.8",
+    python_requires=">=3.6",
+    install_requires=[
+        "requests>=2.25.1",
+        "rich>=10.0.0",
+    ],
     entry_points={
         "console_scripts": [
             "dmrid_lookup=dmrid_lookup:main",
